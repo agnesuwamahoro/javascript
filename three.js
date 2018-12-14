f730e35991cf03ab0a0db5e917efc1dcbe60523a
agnesuwamahoro <magretmuk@gmail.com>
2018-12-14 14:31:14 +0200

    added if

--- three.js
+++ three.js
@@ -9,5 +9,36 @@ function myFunction(){
 
     var thirdSide=document.getElementById("thirdSide").value;
     alert(thirdSide)
+   
 }
-if(is triangle(firstSide,secondSide,thrirdSide)){}
+function myNumber(){
+    var firstSide=parseInt(document.getElementById("firstSide").value);
+    var secondSide=parseInt(document.getElementById("secondSide").value);
+    var thirdSide=parseInt(document.getElementById("thirdSide").value);
+}
+
+
+
+
+    if (firstSide===secondSide&&thrirdSide===firstSide&&secondSide===thirdSide){
+
+alert("Equilateral triangle");
+
+console.log("Equilateral");}
+
+else if("firstSide==secondSide||firstSide==thirdside||secondSide==thirdSide"){
+
+alert("Isoseles triangle");
+
+console.log("Isosele triangle")}
+
+else if (firstSide+secondSide>thirdSide&&firstSide+thirdSide>secondSide&&secondSide+thirdSide>firstSide{
+    
+alert("scale triangle")
+
+console.log("scalene triangle")
+}
+
+else {alert("Not triangle")}
+
+
