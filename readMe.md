0c95cadd96548106098a294bb5dbe94e3741ee30
agnesuwamahoro <magretmuk@gmail.com>
2018-12-18 10:47:53 +0200

    removed submit button

--- readMe.md
+++ readMe.md
@@ -30,10 +30,10 @@ this project is protected under terms of MIT license
 https://github.com/agnesuwamahoro/javascript
 
 ### BDD
-| __Description__                                                                __imput_|  __out put__   |
-|---------------------------------------------------------------------------    |
-| When i put in 3 equal numbers it will says it is  an equilatelal triangle      |  3,3,3    | equilateral |         
-| When i put 2 equal numbers different with one it says it is isocele triangle   |3,3,6      |isocele      |                                     
-| When i put 3 differents numbers it says scalene triangle                       |3,4,7       |scalene     |
+| __Description__                                                                |__imput__   |  __out put__ |
+|--------------------------------------------------------------------------------|---------------------------
+| When i put in 3 equal numbers it will says it is  an equilatelal triangle      |  3,3,3     | equilateral  |         
+| When i put 2 equal numbers different with one it says it is isocele triangle   |3,3,6       |isocele       |                                     
+| When i put 3 differents numbers it says scalene triangle                       |3,4,7       |scalene       |
 
-|When  i put 3 differents things to make a triangle it can not make a triangle   |3,5,B        |not a triangle|
+|When  i put 3 differents things to make a triangle it can not make a triangle   |3,5,B       |not a triangle|
--- three.js
+++ three.js
@@ -6,45 +6,43 @@
     var numbers[1]=document.getElementById("sideTwo").value;
     alert(numbers[1])
 
-
     var numbers[2]=document.getElementById("sideThree").value;
     alert(numbers[2])
    
 }*/
 function myNumber(){
-    var numbers=[sideOne,sideTwo,sideThree]
-    numbers[0]=document.getElementById("sideOne").value;
-    numbers[1]=document.getElementById("sideTwo").value;
-    numbers[2]=document.getElementById("sideThree").value;
+    var numbers= ["sideOne","sideTwo","sideThree"];
+    numbers[0]= parseInt(document.getElementById("sideOne").value);
+    numbers[1]= parseInt(document.getElementById("sideTwo").value);
+    numbers[2]= parseInt(document.getElementById("sideThree").value);
     
 
 
+    if((numbers[0]+numbers[1])>numbers[2] && (numbers[0]+numbers[2])>numbers[1] && (numbers[1]+numbers[2])>numbers[0]){
+        alert("triangle");
+        if (numbers[0]===numbers[1] && numbers[0]===numbers[2] && numbers[1]===numbers[2]){
 
-
-    if (numbers[0]===numbers[1] && numbers[0]===numbers[2] ){
-
-     alert("Equilateral triangle");
-
+            alert("Equilateral triangle");
+       
+           }
+       
+           else if((numbers[0]===numbers[1]) || (numbers[0] ===numbers[2]) || (numbers[1]===numbers[2])){
+       
+           alert("Isoseles triangle");
+       
+          }
+       
+          else {
+            
+           
+          alert("scale triangle")
     }
-
-    else if(numbers[0]===numbers[1] && numbers[0]===numbers[2] && numbers[1]===numbers[2]){
-
-    alert("Isoseles triangle");
-
    }
-
-   else if (numbers[0]+numbers[1]>numbers[2]&&numbers[0]+numbers[2]>numbers[1]&&numbers[1]+numbers[2]>numbers[0]){
-    
-   alert("scale triangle")
-
-
-  }
 
   else {
     alert("Not triangle");
   }
-
-}
+};
 function myReset(){
     document.getElementById("angles").reset();
 }
